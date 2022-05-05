c622f1a634979253606d9d7936386ecf36623ef9
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-05-05 18:25:50 -0400

    added default projects

---
+++ src/components/Main/defaultprojects.js
@@ -0,0 +1,151 @@
+
+export const defaultProjects = [
+    {
+        project: 'health',
+        purpose: 'to not weigh 200 lbs',
+        picture: 'google.com/imgages/bigboi',
+        defaults: [
+            {
+                title: "sleep time",
+                description: "when did you lay in bed",
+                type: 'input',
+                entry: ''
+            },
+            {
+                title: "cups of coffee",
+                description: "how much caffeine",
+                type: 'input',
+                entry: ''
+            },
+            {
+                title: "running",
+                description: "how many minutes did you run",
+                type: 'input',
+                entry: ''
+            }
+        ],
+        entries: [
+            {
+                date: '5/4/2022',
+                categories: [
+                    {
+                        title: "sleep time",
+                        description: "when did you lay in bed",
+                        type: 'input',
+                        entry: '10:00'
+                    },
+                    {
+                        title: "cups of coffee",
+                        description: "how much caffeine",
+                        type: 'input',
+                        entry: '6 cups'
+                    },
+                    {
+                        title: "screen time",
+                        description: "how much blue light",
+                        type: 'input',
+                        entry: '6 hours'
+                    },
+                    {
+                        title: "running",
+                        description: "how many minutes did you run",
+                        type: 'input',
+                        entry: '35'
+                    }]
+            },
+            {
+                date: '5/3/2022',
+                categories: [
+                    {
+                        title: "sleep time",
+                        description: "when did you lay in bed",
+                        type: 'input',
+                        entry: '10:30'
+                    },
+                    {
+                        title: "cups of coffee",
+                        description: "how much caffeine",
+                        type: 'input',
+                        entry: '7 cups'
+                    },
+                    {
+                        title: "running",
+                        description: "how many minutes did you run",
+                        type: 'input',
+                        entry: '30'
+                    }]
+            }
+        ]
+    },
+    {
+        project: 'basketball practice',
+        purpose: 'to have a structure to team practices',
+        picture: 'google.com/imgages/celtics',
+        defaults: [
+            {
+                title: "sleep time",
+                description: "when did you lay in bed",
+                type: 'input',
+                entry: ''
+            },
+            {
+                title: "cups of coffee",
+                description: "how much caffeine",
+                type: 'input',
+                entry: ''
+            },
+            {
+                title: "running",
+                description: "how many minutes did you run",
+                type: 'input',
+                entry: ''
+            }
+        ],
+        entries: [
+            {
+                date: '5/4/2022',
+                categories: [
+                    {
+                        title: "warmup game",
+                        description: "how many buckets",
+                        type: 'input',
+                        entry: '120'
+                    },
+                    {
+                        title: "missed layups",
+                        description: "how many gimme points missed",
+                        type: 'input',
+                        entry: '5'
+                    },
+                    {
+                        title: "time started",
+                        description: "what time did you start?",
+                        type: 'input',
+                        entry: '5:00'
+                    }]
+            },
+            {
+                date: '5/3/2022',
+                categories: [
+                    {
+                        title: "warmup game",
+                        description: "how many buckets",
+                        type: 'input',
+                        entry: '110'
+                    },
+                    {
+                        title: "missed layups",
+                        description: "how many gimme points missed",
+                        type: 'input',
+                        entry: '5'
+                    },
+                    {
+                        title: "time started",
+                        description: "what time did you start?",
+                        type: 'input',
+                        entry: '5:00'
+                    }]
+            }
+        ]
+    }
+]
